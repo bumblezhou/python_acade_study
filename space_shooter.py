2856dd7df099d5417b780b4e075de3c964467aa5
cozhouxiaoke <xiaoke.zhou.bp@china.nttdata.com>
2023-12-20 16:59:48 +0800

    add a book of machine learning

--- space_shooter.py
+++ space_shooter.py
@@ -3,6 +3,9 @@ import arcade
 import random
 
 # Constants
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "SpaceShooter"
 SCALING = 2.0
 
 
@@ -11,57 +14,86 @@ SCALING = 2.0
 # https://arcade.academy/examples/sprite_collect_coins_background.html
 # classes
 class SapceShooter(arcade.Window):
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        # set up the empty sprite list
-        self.enemies_list = arcade.SpriteList()
-        self.clouds_list = arcade.SpriteList()
-        self.all_sprites = arcade.SpriteList()
+        # Our Scene Object
+        self.scene = None
 
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Background image will be stored in this variable
+        self.background = None
+        # self.background = arcade.load_texture("images/universe-wallpaper.png")
+        # self.background = arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg")
         # set the background color
         arcade.set_background_color(arcade.color.SKY_BLUE)
+    
+    def setup(self):
+        # Initialize Scene
+        self.scene = arcade.Scene()
+
+        # Create the Sprite lists
+        self.scene.add_sprite_list("Player")
+
+        # Create the Sprite lists
+        self.scene.add_sprite_list("Enemies")
 
         # set up the player
-        self.player = arcade.Sprite("images/sprite1.png")
-        self.player.center_y = self.height / 2
-        self.player.left = 10
-        self.all_sprites.append(self.player)
+        self.player_sprite = arcade.Sprite("images/sprite1.png")
+        # self.player_sprite.center_y = SCREEN_HEIGHT / 2
+        # self.player_sprite.left = 10
+        self.player_sprite.left = random.randint(SCREEN_WIDTH, SCREEN_WIDTH + 80)
+        self.player_sprite.top = random.randint(10, SCREEN_HEIGHT - 10)
+        self.player_sprite.velocity = (random.randint(-15, -3), 0)
+        self.scene.add_sprite("Player", self.player_sprite)
 
-        self.background = arcade.load_texture("images/universe-wallpaper.png")
+        # # first create the new enemy sprite
+        # enemy = arcade.Sprite("images/missle.png")
+        # # set its position to a random height and off screen right
+        # enemy.left = random.randint(SCREEN_WIDTH, SCREEN_WIDTH + 80)
+        # enemy.top = random.randint(10, SCREEN_HEIGHT - 10)
+
+        # # Set its speed to a random speed heading left
+        # enemy.velocity = (random.randint(-15, -3), 0)
+        # self.scene.add_sprite("Enemies", enemy)
 
         # spawn a new enemy every 0.25 seconds
-        arcade.schedule(self.add_enemy, 0.5)
+        # arcade.schedule(self.add_enemy, 0.5)
 
     def on_draw(self):
-        arcade.start_render()
+        # Clear the screen to the background color
+        self.clear()
 
         # Draw the background texture
-        arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
+        # arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
-        # Draw all sprites
-        arcade.SpriteList.draw(self.all_sprites)
+        # Draw our Scene
+        self.scene.draw()
 
-    def on_update(self, delta_time):
-        # Update all sprites
-        arcade.SpriteList.update(self.all_sprites)
+    def on_update(self):
+        # Draw our Scene
+        self.
 
     def add_enemy(self, delta_time: float):
         # first create the new enemy sprite
         enemy = arcade.Sprite("images/missle.png")
-
         # set its position to a random height and off screen right
-        enemy.left = random.randint(self.width, self.width + 80)
-        enemy.top = random.randint(10, self.height - 10)
+        enemy.left = random.randint(SCREEN_WIDTH, SCREEN_WIDTH + 80)
+        enemy.top = random.randint(10, SCREEN_HEIGHT - 10)
 
         # Set its speed to a random speed heading left
         enemy.velocity = (random.randint(-15, -3), 0)
+        self.scene.add_sprite("Enemies", enemy)
 
-        self.enemies_list.append(enemy)
-        self.all_sprites.append(enemy)
 
+def main():
+    """Main function"""
+    window = SapceShooter()
+    window.setup()
+    arcade.run()
 
 # Main code entry point
 if __name__ == "__main__":
-    app = SapceShooter(960, 600, "Space Shooter")
-    arcade.run()
+    main()
